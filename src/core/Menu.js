2d79b2f9be91ee774c38ce407f67e92fbe058e06
Young Han <hanyc1205@gmail.com>
2020-08-27 20:10:27 -0400

    finished sign in and out

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,34 +1,65 @@
-import React from "react";
+import React, {Fragment} from "react";
 import { Link, withRouter } from "react-router-dom";
+import { signout, isAuthenticated } from "../auth";
 
-const isActive = (history, path) =>{
-    if(history.location.pathname === path){
-        return{color:'#ff9900'}
-    }else{
-        return{color:'#ffffff'}
-    }
-}
+const isActive = (history, path) => {
+  if (history.location.pathname === path) {
+    return { color: "#ff9900" };
+  } else {
+    return { color: "#ffffff" };
+  }
+};
 
 //withRouter can be used history.
 
-function Menu({history}) {
+function Menu({ history }) {
   return (
     <ul className="nav nav-tabs bg-primary">
       <li className="nav-item">
-        <Link className="nav-link" style={isActive(history, '/')} to="/">
+        <Link className="nav-link" style={isActive(history, "/")} to="/">
           Home
         </Link>
       </li>
-      <li className="nav-item">
-        <Link className="nav-link"  style={isActive(history, '/signin')} to="/signin">
-          SignIn
+
+      {!isAuthenticated() && (
+        <Fragment>
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              style={isActive(history, "/signin")}
+              to="/signin"
+            >
+              SignIn
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              style={isActive(history, "/signup")}
+              to="/signup"
+            >
+              SignUp
+            </Link>
+          </li>
+        </Fragment>
+      )}
+
+      {isAuthenticated() && (
+        <li className="nav-item">
+        <Link
+          className="nav-link"
+          style={{ cursor: "pointer", color: "#ffffff" }}
+          onClick={() =>
+            signout(() => {
+              history.push("/");
+            })
+          }
+        >
+          Sign Out
         </Link>
       </li>
-      <li className="nav-item">
-        <Link className="nav-link"  style={isActive(history, '/signup')} to="/signup">
-          SignUp
-        </Link>
-      </li>
+      )}
     </ul>
   );
 }
--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,10 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../core/Layout";
+import { Redirect } from "react-router-dom";
+import { signin, authenticate} from "../auth/index";
 
-export default function Signin() {
+const Signin = () => {
+  const [values, setValues] = useState({
+    email: "han@gmail.com",
+    password: "asdfg1",
+    error: "",
+    loading: false,
+    redirectToReferrer: false,
+  });
+
+  const { email, password, loading, error, redirectToReferrer } = values;
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false, loading: true });
+    signin({ email, password }).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, loading: false });
+      } else {
+        authenticate(data, () => {
+          setValues({
+            ...values,
+            redirectToReferrer: true,
+          });
+        });
+      }
+    });
+  };
+
+  const signUpForm = () => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          onChange={handleChange("email")}
+          type="email"
+          className="form-control"
+          value={email}
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          onChange={handleChange("password")}
+          type="password"
+          className="form-control"
+          value={password}
+        />
+      </div>
+      <button onClick={clickSubmit} className="btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+
+  const showError = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+
+  const showLoading = () =>
+    loading && (
+      <div className="alert alert-info">
+        <h2>Loading...</h2>
+      </div>
+    );
+
+  const redirectUser = () => {
+    if (redirectToReferrer) {
+      return <Redirect to="/" />;
+    }
+  };
+
   return (
-    <Layout title="Sign In" description="Sing In Node React E-commerce App">
-      ...
+    <Layout
+      title="Sign In"
+      description="Sign In Node React E-commerce App"
+      className="container col-md-8 offset-md-2"
+    >
+      {showLoading()}
+      {showError()}
+      {signUpForm()}
+      {redirectUser()}
     </Layout>
   );
-}
+};
+
+export default Signin;
