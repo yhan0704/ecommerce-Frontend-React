94ee3377863a7512b3411a8f8a72c8597b8c5200
yhan0704 <hanyc1205@gmail.com>
2020-09-15 15:46:10 -0400

    working on product sortby and how products

--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -26,3 +26,11 @@ export const createProduct = (userId, token, product) => {
     .then((res) => res.json())
     .catch((err) => console.log(err));
 };
+
+export const getCategories = () => {
+  return fetch(`${API}/categories`, {
+    method: "GET",
+  })
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
+};
--- src/core/Home.js
+++ src/core/Home.js
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
+import { getProducts } from "./apiCore";
+import Card from "./Card";
 
 function Home() {
+  const [productsBySell, setProductBySell] = useState([]);
+  const [productsByArrival, setProductByArrival] = useState([]);
+  const [error, setError] = useState("");
+
+  const loadProductsBySell = () => {
+    getProducts("sold").then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductBySell(data);
+      }
+    });
+  };
+
+  const loadProductsByArrival = () => {
+    getProducts("ceateAt").then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductByArrival(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadProductsByArrival();
+    loadProductsBySell();
+  }, []);
+
   return (
-    <Layout title="Home Page" description="Node React E-commerce App">
-      ...
+    <Layout
+      title="Home Page"
+      description="Node React E-commerce App"
+      className="container-fluid"
+    >
+      <h2 className="mb-4">New Arrivals</h2>
+      <div className="row">
+        {productsByArrival.map((product, i) => (
+          <Card key={i} product={product} />
+        ))}
+      </div>
+
+      <h2 className="mb-4">Best Sellers</h2>
+      <div className="row">
+        {productsBySell.map((product, i) => (
+          <Card key={i} product={product} />
+        ))}
+      </div>
     </Layout>
   );
 }
